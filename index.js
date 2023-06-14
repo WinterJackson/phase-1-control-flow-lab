8809d8a9fa794b2fe67470d6e90101ee135793c3
WinterJackson <winterjacksonwj@gmail.com>
2023-06-14 08:30:17 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+   if (distance <= 400) {
+      return 'This one is on me!';
+    } else if (distance > 400 && distance <= 2500) {
+      return 'I will gladly take your thirty bucks.';
+    } else if (distance > 2500) {
+      return 'No can do.';
+    }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  
+  let message = ""
+  city === "NYC" ? message = "Ok, sounds good." : message = "No go."
+  return message
+  
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+      switch(tip){
+       case 'generous':
+        return `Thank you so much.`
+       case 'not as generous':
+         return `Thank you.`
+       default:
+        return `Bye.`
+      }
 }
